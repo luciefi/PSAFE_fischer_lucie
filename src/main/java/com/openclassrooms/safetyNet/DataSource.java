857855362befd3af1a93bd9186dc3f4d17c17c80
Lucie <lucie.fischer@outlook.com>
2022-01-30 16:55:26 +0100

    read data file from source

---
+++ src/main/java/com/openclassrooms/safetyNet/DataSource.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.safetyNet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.safetyNet.model.FireStation;
+import com.openclassrooms.safetyNet.model.MedicalRecord;
+import com.openclassrooms.safetyNet.model.Person;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Component
+@Data
+public class DataSource {
+    private List<Person> persons;
+    private List<MedicalRecord> medicalrecords;
+    private List<FireStation> firestations;
+
+    @PostConstruct
+    private void initDataSource() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            DataSource dataSource = mapper.readValue(Paths.get("./data.json").toFile(), DataSource.class);
+            persons = dataSource.persons;
+            medicalrecords = dataSource.medicalrecords;
+            firestations = dataSource.firestations;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
